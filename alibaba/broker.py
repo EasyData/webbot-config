f82f2f6cfdb9ca33c85faa7fff3d113014f8518b
Kev++ <vimagick@gmail.com>
2014-05-15 13:45:41 +0800

    update

--- alibaba/broker.py
+++ alibaba/broker.py
@@ -10,22 +10,120 @@ import math
 import pymongo
 import redis
 import sys
+import time
 
 try:
     import cPickle as pickle
 except ImportError:
     import pickle
 
+#########################################
+# -------------- timeline ------------- #
+# all -> pending -> running -> finished #
+#########################################
+
+class ItemParser(object):
+
+    metas = {
+        'alibaba:go:idx': {
+            'mp': 'alibaba:go', # mongo prefix
+            'mk': 'oid',        # mongo key
+            'rp': 'alibaba:go', # redis prefix
+            'rk': 'pending',    # redis key
+            'rv': 'oid',        # redis val
+        },
+        'alibaba:go:detail': {
+            'mp': 'alibaba:go', # mongo prefix
+            'mk': 'oid',        # mongo key
+            'rp': 'alibaba:go', # redis prefix
+            'rk': 'finished',   # redis key
+            'rv': 'oid',        # redis val
+        }
+    }
+
+    def parse(self, data):
+
+        meta = self.metas[data['_meta']]
+        del data['_meta']
+        return data, meta
+
 class RedisDBA(object):
 
     def __init__(self, rdb_uri):
 
-        log.msg('connect: %s'%redis_uri)
+        log.msg('connect: %s'%rdb_uri)
         self.rdb = redis.StrictRedis.from_url(rdb_uri)
 
-    def filter(self, item):
+    def filter(self, item, meta):
 
-        return True
+        rp = meta['rp']
+        rk = meta['rk']
+        rv = meta['rv']
+
+        val = item[rv]
+
+        if rk=='pending':
+
+            skey = '{}:all'.format(rp)
+            sval = val
+
+            if self.rdb.sadd(skey, sval):
+
+                # timeline
+                zkey = '{}:timeline'.format(rp)
+                zval = val
+                zscore = int(time.time())
+                self.rdb.zadd(zkey, zscore, zval)
+
+                # pending
+                lkey = '{}:pending'.format(rp)
+                lval = val
+                self.rdb.rpush(lkey, lval)
+
+                return True
+
+            else:
+
+                # finished
+                zkey = '{}:finished'.format(rp)
+                zval = val
+                if self.rdb.zrank(zkey, zval)!=None:
+                    return False
+
+                # timeline
+                zkey = '{}:timeline'.format(rp)
+                zval = val
+                zscore = self.rdb.zscore(zkey, zval)
+                now = int(time.time())
+                delta = now-zscore
+
+                if delta>3600:
+
+                    # timeline
+                    zscore = now
+                    self.rdb.zadd(zkey, zscore, zval)
+
+                    # pending
+                    lkey = '{}:pending'.format(rp)
+                    lval = val
+                    self.rdb.lpush(lkey, lval)
+                    return True
+
+                else:
+
+                    return False
+
+        elif rk=='finished':
+
+            # finished
+            zkey = '{}:finished'.format(rp)
+            zval = val
+            zscore = int(time.time())
+            return self.rdb.zadd(zkey, zscore, zval)
+
+        else:
+
+            return True
 
 class MongoDBA(object):
 
@@ -37,24 +135,12 @@ class MongoDBA(object):
         db, tbl = parsed['database'], parsed['collection']
         self.mdb = pymongo.MongoClient(host, port)[db][tbl]
 
-    def get_pk(self, item):
-
-        if '_pk' in item:
-            pk = item['_pk']
-            del item['_pk']
-            return pk
-
-        for pk in ['oid', 'mid']:
-            if pk in item:
-                return pk
-
-    def save(self, item):
+    def save(self, item, meta):
 
         try:
-            pk = self.get_pk(item)
-            if pk:
-                pv = item[pk]
-                self.mdb.update({pk:pv}, item, upsert=True)
+            mk = meta.get('mk')
+            if mk:
+                self.mdb.update({mk:item[mk]}, item, upsert=True)
             else:
                 self.mdb.insert(item)
         except:
@@ -73,14 +159,16 @@ class Broker(object):
         self.cnn.onPull = self.process
         self.rdba = RedisDBA(rdb_uri)
         self.mdba = MongoDBA(mdb_uri)
+        self.parse = ItemParser().parse
 
     def process(self, msg):
 
         try:
             buf = msg[0]
-            item = pickle.loads(buf)
-            if self.rdba.filter(item):
-                self.mdba.save(item)
+            data = pickle.loads(buf)
+            item, meta = self.parse(data)
+            if self.rdba.filter(item, meta):
+                self.mdba.save(item, meta)
         except:
             log.err()
 
@@ -93,5 +181,8 @@ if __name__=='__main__':
     #log.startLogging(DailyLogFile.fromFullPath("/tmp/alibaba-broker.log"))
     log.startLogging(sys.stdout)
 
-    Broker('tcp://*:1688').run()
+    zmq_uri = 'tcp://*:1688'
+    rdb_uri = 'redis://localhost:6379/9'
+    mdb_uri = 'mongodb://localhost:27017/alibaba.go'
+    Broker(zmq_uri, rdb_uri, mdb_uri).run()
 
